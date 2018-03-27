8e3f86b2c3574b00512cfa8a63b319daa432da35
Francis <Yuweh@users.noreply.github.com>
2018-03-27 08:15:57 +0800

    to be updated

---
+++ CORE-ML/ReadMe.swift
@@ -0,0 +1,7 @@
+# CORE ML
+
+Core ML a foundational machine learning framework used across Apple products, including Siri, Camera, and QuickType.
+
+Projects 
+
+1. Yummy! - a "SeeFood" Clone App used [InceptionV3 Model] to recognize hotdog image
